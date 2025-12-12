95f56040676e22cd2cf2cd6941e42c1e896a8329
yamauchi_yui <yamauchi.yui@alhinc.jp>
2025-12-12 13:37:15 +0900

    実践問題その②実装後

--- src/chapter6/dao/UserMessageDao.java
+++ src/chapter6/dao/UserMessageDao.java
@@ -32,7 +32,7 @@ public class UserMessageDao {
 
 	}
 
-	public List<UserMessage> select(Connection connection, int num) {
+	public List<UserMessage> select(Connection connection, Integer id, int num) {
 
 		log.info(new Object(){}.getClass().getEnclosingClass().getName() + 
 		" : " + new Object(){}.getClass().getEnclosingMethod().getName());
@@ -50,10 +50,21 @@ public class UserMessageDao {
 			sql.append("FROM messages ");
 			sql.append("INNER JOIN users ");
 			sql.append("ON messages.user_id = users.id ");
+			//idがnull以外
+			if (id != null) {
+				sql.append("WHERE messages.user_id = ? ");
+			}
+			
 			sql.append("ORDER BY created_date DESC limit " + num);
 
 			ps = connection.prepareStatement(sql.toString());
+			
+			 // messages.user_idにidをセット
+			if(id != null) {
+				ps.setInt(1, id);
+			}
 
+			//結果をセット
 			ResultSet rs = ps.executeQuery();
 
 			List<UserMessage> messages = toUserMessages(rs);
